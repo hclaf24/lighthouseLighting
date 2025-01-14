1d5b8cce702b67ffadf58d2129c42e305d63d472
Hannah Claflin <hannahclaflin98@gmail.com>
2025-01-14 12:50:48 -0600

    Remove max width to center section on lights page

--- src/pages/Lights.tsx
+++ src/pages/Lights.tsx
@@ -2,7 +2,7 @@ import data from "../data/index.json";
 
 const Lights = () => {
   return (
-    <section id="Lights" className="flex p-8 flex-col justify-self-center max-w-6xl">    
+    <section id="Lights" className="flex p-8 flex-col justify-self-center">    
       <h1 className="text-center py-8 text-charcoal">Lights</h1>
       <h2 className="bg-yinmn-blue text-white mt-8 p-4 rounded-lg" id="indoorLights">Indoor Lights</h2>
       <div className="p-8 flex flex-row gap-8 flex-wrap justify-center">
